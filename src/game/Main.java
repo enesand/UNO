4968d38af77a5f818c84460246c4c5e6440b0df9
Muhammed Enes Andiç <147348466+enesand@users.noreply.github.com>
2024-06-10 03:31:02 +0300

    Update Main.java

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,12 +1,4 @@
-/************** Pledge of Honor ******************************************
-I hereby certify that I have completed this programming project on my own 
-without any help from anyone else. The effort in the project thus belongs 
-completely to me. I did not search for a solution, or I did not consult any 
-program written by others or did not copy any program from other sources. I 
-read and followed the guidelines provided in the project description.
-READ AND SIGN BY WRITING YOUR NAME SURNAME AND STUDENT ID 
-SIGNATURE: <Muhammed Enes Andiç, 83469>
-*************************************************************************/
+
 package game;
 
 import java.awt.EventQueue;
